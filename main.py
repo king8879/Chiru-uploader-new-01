3720e1d0604405e0af9ea97a660959e0f0be804c
Arjun <99074495+Wuhuiuusushsg@users.noreply.github.com>
2025-04-20 01:07:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -288,7 +288,7 @@ async def account_login(bot: Client, m: Message):
     subscription_data = read_subscription_data()
     if not any(user[0] == user_id for user in subscription_data):
         await m.reply_text("❌ You are not a premium user. Please upgrade your subscription! 💎")
-
+        return 
 async def arjun_command(client, message: Message):
     chat_id = message.chat.id
     await message.reply_text
